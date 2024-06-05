fe1d0be4abcebcab18b5813bc67bd77c06ef19d0
Mischa <michael.sumy@gmx.at>
2024-06-05 18:30:24 +0200

    ProfileCreator: Implemented the creation and the Loading of the database at the Start of the Programm.

--- ProfileCreator/connection.h
+++ ProfileCreator/connection.h
@@ -4,6 +4,8 @@
 #include <QtSql/QSqlDatabase>
 #include<QtSQl/QSqlQuery>
 #include <QMessageBox>
+#include "QFile"
+#include "QDebug"
 
 
 static bool createconnection()
@@ -11,7 +13,28 @@ static bool createconnection()
     // Ausgabe der Typen von Datenbanken
     qDebug() << QSqlDatabase::drivers();    // Liefert eine Liste von Treibern
     // Anlegen eines Datenbankobjektes
+    QString dbName = "Profiles.db";   //
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("Profiles.db");
+    if( QFile::exists(dbName))
+    {
+        QMessageBox msg;
+        msg.setText("Database *Profiles.db* has been loaded!");
+        msg.setWindowTitle("Info");
+        msg.addButton("Ok",QMessageBox::YesRole);
+        msg.exec();
+    if(!db.open())
+        {
+            QMessageBox msg;
+            msg.setText("Unable to connect to *Profiles.db* !");
+            msg.setWindowTitle("Error");
+            msg.addButton("OK", QMessageBox::YesRole);
+            msg.exec();
+            return false;
+        }
+    }
+    else
+    {
     db.setDatabaseName("Profiles.db");
     if(!db.open())
     {
@@ -22,6 +45,12 @@ static bool createconnection()
         msg.exec();
         return false;
     }
+    QMessageBox msg;
+    msg.setText("Setting up new Database *Profiles.db*");
+    msg.setWindowTitle("Info");
+    msg.addButton("Ok",QMessageBox::YesRole);
+    msg.exec();
+
     // Tabellen anlegen
     QSqlQuery query;
     query.exec("drop table Profiles");
@@ -44,7 +73,15 @@ static bool createconnection()
                            primary key ('PID' autoincrement) \
                            foreign key ('FilterFK') references 'Filters' ('FID'))\
                 ");
+    query.exec("delete from Profiles");
+    query.exec("delete from Filters");
+    query.exec("insert into Filters (FName) values ('AVR')");
+    query.exec("insert into Filters (FName) values ('MED')");
+    query.exec("insert into Filters (FName) values ('MIX')");
+    query.exec("insert into Profiles (PName,MINrpm,MAXrpm,SNumber,FilterFK) values ('Default','30','80','6', 1)");
 
+
+/*
                 // Testdaten    => solange man entwickelt
                 // wenn das Programm fertig ist, kann dieser Abschnitt gelöscht werden
     query.exec("delete from Profiles");
@@ -61,6 +98,9 @@ static bool createconnection()
     query.exec("delete from Profiles where PName = 'Dirt_Slow'");
     query.exec("insert into Profiles (PName,MINrpm,MAXrpm,SNumber,FilterFK) values ('Assetto F1','30','85','4', 1)");
     query.exec("insert into Profiles (PName,MINrpm,MAXrpm,SNumber,FilterFK) values ('Dirt_Slow','15','80','10', 4)");
+*/
+    return true;
+    }
     return true;
 }
 
